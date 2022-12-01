2ef5c5555918bfabd80a36a3d3ae660424de3485
jsliew <jsliew2000@gmail.com>
2022-12-01 18:46:02 +0800

    Added mouse input to the InputManager.

--- BulletManiac/Managers/InputManager.cs
+++ BulletManiac/Managers/InputManager.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace BulletManiac.Managers
 {
@@ -8,6 +9,7 @@ namespace BulletManiac.Managers
     /// </summary>
     public static class InputManager
     {
+        private static MouseState lastMouseState;
         private static Vector2 direction;
         /// <summary>
         /// Walk direction for the player
@@ -17,20 +19,30 @@ namespace BulletManiac.Managers
         /// Is the player moving
         /// </summary>
         public static bool Moving => direction != Vector2.Zero;
+        public static Vector2 MousePosition { get; private set; }
+        public static bool MouseLeftClick { get; private set; }
 
         public static void Update(GameTime gameTime)
         {
+            // Keyboard Update
             direction = Vector2.Zero;
 
             KeyboardState keyboardState = Keyboard.GetState();
 
             if(keyboardState.GetPressedKeyCount() > 0)
             {
+                // Increment / decrement based on the input
                 if (keyboardState.IsKeyDown(Keys.A)) direction.X--;
                 if (keyboardState.IsKeyDown(Keys.D)) direction.X++;
                 if (keyboardState.IsKeyDown(Keys.W)) direction.Y--;
                 if (keyboardState.IsKeyDown(Keys.S)) direction.Y++;
             }
+
+            // Mouse Update
+            MouseState mouseState = Mouse.GetState();
+            MousePosition = mouseState.Position.ToVector2(); // Position Update
+            MouseLeftClick = mouseState.LeftButton == ButtonState.Pressed && lastMouseState.LeftButton == ButtonState.Released; // The boolean will only register once
+            lastMouseState = mouseState; // Store the last mouse state for the comparison
         }
     }
 }
