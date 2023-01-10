ff79edab86f8b8437a32d1a75a5a1612339cf414
jsliew <jsliew2000@gmail.com>
2023-01-10 12:11:33 +0800

    Add basic scene manager code.

---
+++ BulletManiac/Scenes/Scene.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletManiac.Scenes
+{
+    public abstract class Scene
+    {
+        /// <summary>
+        /// Determine if this state need to update
+        /// </summary>
+        public bool Pause { get; set; }
+        /// <summary>
+        /// Determine if this state need to draw
+        /// </summary>
+        public bool Drawing { get; set; }
+
+        public Scene()
+        {
+
+        }
+
+        public abstract void LoadContent();
+        public abstract void UnloadContent();
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(SpriteBatch spriteBatch, GameTime gameTime);
+    }
+}
---
+++ BulletManiac/Scenes/SceneManager.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletManiac.Scenes
+{
+    public static class SceneManager
+    {
+        private static List<Scene> states = new();
+        private static int currentStateIndex = 0;
+
+        public static void Add(Scene state)
+        {
+            states.Add(state);
+        }
+
+        public static void Update(GameTime gameTime)
+        {
+            states[currentStateIndex].Update(gameTime);
+        }
+
+        public static void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            states[currentStateIndex].Draw(spriteBatch, gameTime);
+        }
+    }
+}
