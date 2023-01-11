a7d78aee1cd1a432290f7af0902f9c66f9ee3a98
jsliew <jsliew2000@gmail.com>
2023-01-12 00:41:32 +0800

    Add powerup base class

---
+++ BulletManiac/Entity/Players/PowerUp.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletManiac.Entity.Players
+{
+    public abstract class PowerUp : GameObject
+    {
+        public PowerUp(Texture2D icon) : base(icon)
+        {
+            
+        }
+
+        protected abstract void PowerUpAction();
+
+        protected override Rectangle CalculateBound()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CollisionEvent(GameObject other)
+        {
+            if(other.Name == "Player")
+            {
+                PowerUpAction();
+                Destroy(this);
+                // Play sound
+            }
+            base.CollisionEvent(other);
+        }
+    }
+}
